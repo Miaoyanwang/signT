fcda25331208638c31bf4ed142064752124acea0
miaoyan wang <saplingwang@gmail.com>
2021-01-01 23:03:45 -0600

    update

--- code/example1.R
+++ code/example1.R
@@ -1,13 +1,13 @@
 source("signT.R")
+library(ggplot2)
 set.seed(1)
 d=30;r=3;
 data_full=NULL
 for(nsim in 1:10){
 a=matrix(rnorm(d*r),ncol=r)
 T=tensorize(a,a,a)
-data_full=NULL
 c=c(0.1,0.5,1,3,5,10,15,20,25,30,35,40,45,50,75,100,125,150,175,200)
-rank=rank2=accuracy=NULL
+rank2=accuracy=NULL
 for(i in 1:20){
 T_trans=1/(1+exp(-(c[i]*T)))
 T_trans=as.tensor(T_trans/sqrt(sum(T_trans^2)))
@@ -19,7 +19,14 @@ data=data.frame(c,rank2)
 colnames(data)=c("c","rank")
 data_full=rbind(data_full,data)
 }
-save(data,fild="example1.RData")
-pdf("example1.pdf",width=3.5,height=3)
-ggplot(data,aes(c,rank))+geom_point(aes(c,rank))+geom_line(aes(c,rank))
-dev.off
+data_full=cbind(data_full,rep(1:10,rep(20,10)))
+colnames(data_full)=c("c","rank","sim")
+save(data_full,file="example1.RData")
+
+ag <- aggregate(. ~ c, data_full, function(x) c(mean = mean(x), sd = sd(x)))
+
+plotc=c(1:8,10,12,14:20)
+ag=ag[plotc,]
+pdf("example1.pdf",width=3.5,height=3.1)
+ggplot(ag,aes(c,rank[,1]))+geom_point(aes(c,rank[,1]))+geom_line(aes(c,rank[,1]))+geom_errorbar(aes(ymin=rank[,1]-rank[,2]/sqrt(10), ymax=rank[,1]+rank[,2]/sqrt(10)), width=.2,position=position_dodge(.9))+labs(x = "transformation level (c)")+labs(y = "numerical rank")
+dev.off()
--- code/example2.R
+++ code/example2.R
@@ -1,4 +1,5 @@
 source("signT.R")
+library(ggplot2)
 set.seed(1)
 d=30;
 Z=T=array(0,dim=c(d,d,d))
@@ -6,7 +7,7 @@ for(i in 1:d){
     for(j in 1:d){
         for(k in 1:d){
         Z[i,j,k]=max(c(i,j,k))/d
-        T[i,j,k]=log10(1+Z[i,j,k])
+        T[i,j,k]=log(1+Z[i,j,k])
     }
 }
 }
@@ -16,10 +17,10 @@ res2=cp(as.tensor(T),30)
 data=cbind(1:30,sort(res$lambda,decreasing=TRUE))
 data=rbind(data,cbind(1:30,sort(res2$lambda,decreasing=TRUE)))
 colnames(data)=c("component","singular value")
-type=as.factor(c(rep("Z",30),rep("T",30)))
-data[,2]=log10(data[,2])
-data=data.frame(data,type)
+tensor=as.factor(c(rep("Z",30),rep("T",30)))
+data[,2]=log(data[,2])
+data=data.frame(data,tensor)
 
 pdf("example2.pdf",width=4,height=3)
-ggplot(data,aes(component,singular.value))+geom_point(aes(component,singular.value,shape=type,col=type))+ylim(2.5,4.5)
+ggplot(data,aes(component,singular.value))+geom_point(aes(component,singular.value,shape=tensor,col=tensor))+ylim(6,10.1)+labs(x = "index")+labs(y = "singular value on log scale")
 dev.off()
